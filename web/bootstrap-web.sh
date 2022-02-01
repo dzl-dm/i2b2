834aba16a11f6e7f65fc1945102af47f1b5f41be
Marc Griffiths <marc.griffiths@innere.med.uni-giessen.de>
2022-02-01 17:05:25 +0100

    WIP: re-introduce most changes

--- web/bootstrap-web.sh
+++ web/bootstrap-web.sh
@@ -47,12 +47,12 @@ echo -e "Env vars:\n $(env)"
 ## Customisation
 echo >&2 "Setting custom settings..."
 cd /var/www/html/
-# find  . -maxdepth 2 -type f -name i2b2_config_data.js -exec sed -i "s*name: \"HarvardDemo\",*name: \"${ORGANISATION_NAME}\",*g" {} \;
-# sed -i 's/allowAnalysis: true,/allowAnalysis: true,\n\t\t  adminOnly: true,/g' admin/i2b2_config_data.js
-# sed -i "s/#ServerName www.example.com:80/#ServerName www.example.com:80\nServerName ${WEB_FQDN}/g" /etc/httpd/conf/httpd.conf
-# if [ ${INCLUDE_DEMO_DATA} != "True" ]; then
-#     find  . -maxdepth 3 -type f -name i2b2_ui_config.js -exec sed -i 's/loginDefaultUsername : "demo"/loginDefaultUsername : ""/g' {} \; -exec sed -i 's/loginDefaultPassword : "demouser"/loginDefaultPassword : ""/g' {} \;
-# fi
+find  . -maxdepth 2 -type f -name i2b2_config_data.js -exec sed -i "s*name: \"HarvardDemo\",*name: \"${ORGANISATION_NAME}\",*g" {} \;
+sed -i 's/allowAnalysis: true,/allowAnalysis: true,\n\t\t  adminOnly: true,/g' admin/i2b2_config_data.js
+sed -i "s/#ServerName www.example.com:80/#ServerName www.example.com:80\nServerName ${WEB_FQDN}/g" /etc/httpd/conf/httpd.conf
+if [ ${INCLUDE_DEMO_DATA} != "True" ]; then
+    find  . -maxdepth 3 -type f -name i2b2_ui_config.js -exec sed -i 's/loginDefaultUsername : "demo"/loginDefaultUsername : ""/g' {} \; -exec sed -i 's/loginDefaultPassword : "demouser"/loginDefaultPassword : ""/g' {} \;
+fi
 # if timeout 15 ping -c 2 i2b2-api ; then
 #     echo >&2 "api server is available, adding proxy to apache config"
 #     cp /docker-entrypoint/conf/i2b2-api.conf /etc/httpd/conf.d/
